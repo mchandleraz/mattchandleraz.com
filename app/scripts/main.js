ae64dc8a8d40d7e09129a2b4942669a363d3c8e7
mchandleraz <info@mattchandleraz.com>
2014-04-19 11:56:59 -0700

    Creates mcaz object, and breakpoints

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+var mcaz = mcaz || {};
+
+mcaz["_breakpoints"] = jRespond([
+    {
+        label: 'base',
+        enter: 0,
+        exit: 479
+    },{
+        label: 'xsmall',
+        enter: 480,
+        exit: 767
+    },{
+        label: 'small',
+        enter: 768,
+        exit: 991
+    },{
+        label: 'medium',
+        enter: 992,
+        exit: 1199
+    },{
+        label: 'large',
+        enter: 1200,
+        exit: 10000
+    }
+]);
+
+mcaz.breakpoint = function () {
+	return mcaz["_breakpoints"].getBreakpoint();
+}
